556184bc29aa2f9da906d5eab346748d7f417b94
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-11-11 15:05:59 +0900

    검색 api 수정

--- src/api/search.api.ts
+++ src/api/search.api.ts
@@ -2,14 +2,20 @@ import axios from 'axios';
 import { ISummary } from './interfaces';
 import { getUrl } from './tools/host';
 
-export const fetchAll = async (page: Number): Promise<ISummary[]> => {
+/**
+ * @description 검색을 실행합니다
+ * @param search 검색어 입니다. 반드시 주어져야 합니다
+ * @param page 현재 페이지입니다. 1을 기본값으로 갖습니다
+ * @param cnt 한번에 보여줄 갯수입니다 10을 기본값으로 갖습니다
+ */
+export const exec = async (search: string, page: Number = 1, cnt: Number = 10): Promise<ISummary[]> => {
 	const response = await axios({
 		method: 'get',
 		url: getUrl('/api/summary'),
 		params: {
-			search: '',
-			page: 1,
-			cnt: 10,
+			search: search,
+			page: page,
+			cnt: cnt,
 		},
 	});
 
