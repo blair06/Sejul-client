bfbf532ce8361f8450fcc9aded8c62839de9e0c2
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-11-13 16:23:34 +0900

    유저 인터페이스 변경

--- src/api/interfaces/IUser.ts
+++ src/api/interfaces/IUser.ts
@@ -1,10 +1,13 @@
 import { ICollection } from './ICollection';
 import { IHashtag } from './IHashtag';
+import { IArticle } from './IArticle';
 
 /**
  * Author       : 유경수
  * Create Date  : 2020-10-14
  * Description  : 유저 정보
+ * History
+ * 2020-11-13 담은 기사 정보 추가
  */
 export interface IUser extends ICollection {
 	email: string; // 사용자 이메일
@@ -17,7 +20,8 @@ export interface IUser extends ICollection {
 		google?: string; // 구글 로그인 토큰
 		naver?: string; // 네이버 로그인 토큰
 	};
-	profile: string; // 프로필 이미지 경로
+	profile: string; // 프로필 이미지 주소
+	articles: [IArticle]; // 담은 기사
 	following: [IUser]; // 내가 팔로우 중인 유저
 	follower: [IUser]; // 나를 팔로우 중인 유저
 	hashtags: [IHashtag]; // 내가 관심 가진 분야
