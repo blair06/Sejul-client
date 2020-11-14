1cf81ea2fba95edaf37b317bc410e5baef437edc
blair06 <hyewon980610@gmail.com>
2020-11-14 20:43:30 +0900

    features/#10 Card 수정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React, {ReactChild, ReactNode, useEffect, useState} from 'react'
 import './Card.scss'
 
 interface CardProps {
-    children: any;
+    children: ReactNode;
     className?: string;
-    onClick?: string;
-    //온클릭시 url
+    onClick?: Function;
+    
 }
+// how to use
+// 박스안 내용들은 Card태그 사이 children을 이용해 담아줍니다
+// prop로 className을 주어 scss를 조정할 수 있습니다
+// 클릭 후 이동이 필요할시 onClick prop을 통해 함수전달이 가능합니다
+
 const Card = (prop: CardProps) => {
     const {children, className, onClick} = prop;
     return (
-        <div className={`__Card-main-container ${className || ''}`} >
+        <div className={`__Card-main-container ${className || ''}`} onClick={() => {
+            if(onClick !== null && onClick !== undefined){
+                onClick();
+                }}}>
             {children}
         </div>
     )
