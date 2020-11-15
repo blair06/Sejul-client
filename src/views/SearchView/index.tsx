0ee4ca80852f6bf2295298777a4cb349c5fbe455
Park_Mi_Ran <gpfk6636@naver.com>
2020-11-16 03:31:20 +0900

    수정2차

--- src/views/SearchView/index.tsx
+++ src/views/SearchView/index.tsx
@@ -5,7 +5,6 @@ import * as API from '../../api'
 import { CustomButton } from '../../components'
 import './scss/SearchView.scss'
 
-
 const SearchView = () => {
     const [items,setItems] = useState<any[]>([]);
     const fn = async () => {
@@ -23,28 +22,29 @@ const SearchView = () => {
                 <input 
                     type="input" 
                     placeholder="검색어를 입력 하세요." 
-                    className="search-bar-input"/> 
-                    <button className="search-button-img" />
+                    className="search-bar-input"/>
+                    <button className="search-button-img-wrap" ><img className="search-button-img" /></button>
             </div>  
             <div className = "wrap-search-button-bar">
                 <button type="button" className = "search-but-news"  onClick={() => console.log('clicked')}>기사검색</button>
                 <button type="button" className = "search-but-post"  onClick={() => console.log('clicked')}>글검색</button>
             </div>
             <div className = "search-container">
-                <div className = "search-card">
             {
                 items.map((item,idx)=> {
                     return(
+                        <div className = "search-card">
                         <div key={idx}>
                             <p className="news-title">{item.article.title}</p>
                             <p className="news-content">{item.content}</p>
                             <p className="news-time">{item.createdAt}</p>
 
                         </div>
+                        </div>
                     )
                 })
             }
-                </div>
+                
             </div>
             </div>
         </>  
