85700285f63e8967872e8f6a53f46f61e8938bd1
blair06 <hyewon980610@gmail.com>
2020-11-16 00:30:11 +0900

    features/#10 main page start

--- src/views/MainView/index.tsx
+++ src/views/MainView/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import * as API from '../../api';
 import { Logo, Card ,RoundedCard } from '../../components';
 import './scss/MainCommon.scss';
+import mainBanner from '../../assets/img/main-banner.png';
 
 const MainView = () => {
     const test = async () => {
@@ -13,13 +14,41 @@ const MainView = () => {
     }, [])
     return (
         <div className="main-view-container">
-            <Logo />
-            {/*  */}
-            <Card>
-            </Card>
-            <RoundedCard className = "add" cancelable = {true}>
-                #해시태그
-            </RoundedCard>
+            <div className="main-banner">
+                <img src={mainBanner}></img>
+            </div>
+            <div className="main-contents-container">
+                <p className="main-contents-header">최근에 작성된 글</p>
+                <p className="main-contents-header">해시 태그</p>
+                <div className="main-hashtag">
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                <RoundedCard className = "add" cancelable = {true}>
+                    #해시태그
+                </RoundedCard>
+                    
+                </div>
+                <p className="main-contents-header">활동이 많은 사용자들</p>
+            </div>
         </div>
     )
 }
