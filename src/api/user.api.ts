7641ac4a128960209244e538dfffcf9befd7a18c
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-11-17 20:51:18 +0900

    유저 페이지 관련 api

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,5 +1,27 @@
 import axios from 'axios';
-import { IAPIResponse, ISummary } from './interfaces';
+import { IAPIResponse, IHashtag, ISummary, IUser } from './interfaces';
 import { getUrl } from './tools/host';
 
-export {};
+export const fetchFollowingUser = async (username: string): Promise<IUser[]> => {
+	const result = await axios({
+		method: 'GET',
+		url: getUrl(`api/${username}/following`),
+	});
+	return result.data;
+};
+
+export const fetchFollowingHashtag = async (username: string): Promise<IHashtag[]> => {
+	const result = await axios({
+		method: 'GET',
+		url: getUrl(`api/${username}/hashtags`),
+	});
+	return result.data;
+};
+
+export const fetchLikeSummary = async (username: string): Promise<ISummary[]> => {
+	const result = await axios({
+		method: 'GET',
+		url: getUrl(`api/${username}/likes`),
+	});
+	return result.data;
+};
