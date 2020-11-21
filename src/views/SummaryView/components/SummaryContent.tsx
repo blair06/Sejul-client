b01780bfef259773733f074907dba92bfba5187a
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-11-21 19:43:44 +0900

    컨텐츠 입력완료

--- src/views/SummaryView/components/SummaryContent.tsx
+++ src/views/SummaryView/components/SummaryContent.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SummaryContent = () => {
+
+interface ISummaryContentProps {
+    content: string;
+    onChange: (content: string) => void;
+}
+
+const SUMMARAY_CONTENT_MAX_LEN = 3000;
+
+const SummaryContent = (props: ISummaryContentProps) => {
+    const { content, onChange } = props;
+    const [len, setLen] = useState(0);
+    const updateContent = (t: string) => {
+        if (t.length > SUMMARAY_CONTENT_MAX_LEN) {
+            alert(`내용은 ${SUMMARAY_CONTENT_MAX_LEN}자를 넘을 수 없습니다`);
+            return;
+        }
+        onChange(t);
+        setLen(t.length);
+    }
     return (
         <div className="__summary-content-container">
-            <textarea className="__summary-content" placeholder="내용을 입력하세요" >
+            <textarea className="__summary-content" placeholder="내용을 입력하세요" value={content}
+                onChange={(e) => updateContent(e.target.value)}>
             </textarea>
-            <p className="__summary-content-length">23/3000자</p>
+            <p className="__summary-content-length">{len}/3000자</p>
         </div>
     )
 }
--- src/views/SummaryView/index.tsx
+++ src/views/SummaryView/index.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { CustomButton } from '../../components';
 import SummaryContent from './components/SummaryContent';
 import ArticleCard from './components/ArticleCard';
 import TimerCard from './components/TimerCard';
 import HashtagCard from './components/HashtagCard';
 
-
 import './scss/summary.scss';
 
 const SummaryView = () => {
+    const matches = useRouteMatch();
+
+    const [content, setContent] = useState("");
+
     return (
         <div className="__summary-view-container">
             <div className="__summary-view-wrapper">
-                <SummaryContent />
+                {/* 요약 내용 작성 */}
+                <SummaryContent content={content} onChange={(content) => { setContent(content) }} />
                 <div className="__summary-components-container">
                     <div className="__summary-components-wrapper">
                         {/* 기사 정보 카드  */}
@@ -21,6 +26,7 @@ const SummaryView = () => {
                         <TimerCard />
                         {/* 해시태그 카드 */}
                         <HashtagCard />
+                        {/* 저장 버튼 */}
                         <CustomButton className="__article-submit-btn" text="저장" onClick={() => { }} />
                     </div>
                 </div>
